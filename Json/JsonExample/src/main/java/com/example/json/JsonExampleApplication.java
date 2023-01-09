e93cd6cd0201149f17fa9f3d35845335f5988fe0
ksnx3684 <84880772+ksnx3684@users.noreply.github.com>
2023-01-09 23:00:08 +0900

    JSON Object 파싱

--- Json/JsonExample/src/main/java/com/example/json/JsonExampleApplication.java
+++ Json/JsonExample/src/main/java/com/example/json/JsonExampleApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.json.service.CompanyService;
+import com.example.json.vo.Company;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -22,11 +23,36 @@ public class JsonExampleApplication {
 		String companyJsonStr = companyService.getCompany();
 		System.out.println(companyJsonStr);
 		
+		
+		
+		/* Json String -> Map */
 		System.out.println("\n1. JsonStr -> Map");
 		Map<String, Object> companyMap = mapper.readValue(companyJsonStr, new TypeReference<Map<String, Object>>() {});
 		System.out.println("회사명: " + companyMap.get("name"));
 		System.out.println("임직원 수: " + (int) companyMap.get("number_of_employees"));
 		System.out.println("서비스: " + companyMap.get("services"));
+		
+		/* Json String -> VO */
+		System.out.println("\n2. JsonStr -> VO");
+		Company company = mapper.readValue(companyJsonStr, Company.class);
+		System.out.println("회사명: " + company.getName());
+		System.out.println("임직원 수: " + company.getNumberOfEmployees());
+		System.out.println("서비스: " + company.getServices());
+		
+		/* Map -> VO */
+		System.out.println("\n3. Map -> VO");
+		Company company2 = mapper.convertValue(companyMap, new TypeReference<Company>() {});
+		System.out.println("회사명: " + company2.getName());
+		System.out.println("임직원 수: " + company2.getNumberOfEmployees());
+		System.out.println("서비스: " + company2.getServices());
+		
+		/* VO -> Json String */
+		System.out.println("\n4. VO -> JsonStr");
+		String jsonStr = mapper.writeValueAsString(companyMap);
+		System.out.println(jsonStr);
+		
+		
+		
 	}
 
 	public static void main(String[] args) throws IOException{
--- Json/JsonExample/src/main/java/com/example/json/vo/Company.java
+++ Json/JsonExample/src/main/java/com/example/json/vo/Company.java
@@ -2,11 +2,16 @@ package com.example.json.vo;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
-@Data
+@Data // getter, setter 및 final 변수가 포함된 생성자 등 다양한 어노테이션들을 묶어 놓음
+@JsonIgnoreProperties(ignoreUnknown = true) // 인식 불가능한 필드가 온 경우 무시한다
 public class Company {
 	private String name;			// 회사명
+	@JsonProperty(value = "number_of_employees") // number_of_employees 필드를 numberOfEmployees 필드로 매핑
 	private int numberOfEmployees;	// 임직원 수
 	private List<String> services;	// 서비스들
 }
